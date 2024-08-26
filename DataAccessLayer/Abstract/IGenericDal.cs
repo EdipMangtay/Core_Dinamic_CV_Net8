86ae27378469f2e8510d26403760bdb42e769621
Ali Edip Mangtay <mangtay0133@gmail.com>
2024-08-26 23:13:05 +0300

    IGenericDal Added

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T p); // burada T tipinde bir parametre alacak ve bu parametreyi p olarak adlandıracak
+        void Update(T p); // burada T tipinde bir parametre alacak ve bu parametreyi p olarak adlandıracak
+        void Delete(T p);
+        List<T> GetList();
+        T GetById(int id);
+    }
+}
